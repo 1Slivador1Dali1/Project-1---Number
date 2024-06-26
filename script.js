527901afeea84afe35ef8f65e497f344945c2e58
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-06-26 21:42:18 +0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-'use strict';
+"use strict";
+
+// console.log(document.querySelector('.section__right_message').textContent);
+// document.querySelector('.section__right_message').textContent = 'Правильное число!';
+
+// document.querySelector('.header__number').textContent = 11;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.section__left_guess').value = 5;
+
+// const x = function() {
+//     console.log(document.querySelector('.section__left_guess').value);
+// };
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.header__number').textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".section__left_guess").value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.section__right_message').textContent = 'Нет числа!';
+  }
+  else if (guess === secretNumber) {
+    document.querySelector('.section__right_message').textContent = 'Вы угадали!';
+  }
+  else if (guess > secretNumber) {
+    document.querySelector('.section__right_message').textContent = 'Загаданное число меньше вашего!';
+  }
+  else if (guess < secretNumber) {
+    document.querySelector('.section__right_message').textContent = 'Загаданное число больше вашего!';
+  }
+});
