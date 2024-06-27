30d787486b093d6c57814d92ae9e7bda2e3b7ed9
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-06-27 18:29:46 +0300

    logic game 1

--- script.js
+++ script.js
@@ -13,7 +13,9 @@
 // };
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.header__number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector(".header__number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".section__left_guess").value);
@@ -21,15 +23,33 @@ document.querySelector(".check").addEventListener("click", function () {
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.section__right_message').textContent = 'Нет числа!';
-  }
-  else if (guess === secretNumber) {
-    document.querySelector('.section__right_message').textContent = 'Вы угадали!';
-  }
-  else if (guess > secretNumber) {
-    document.querySelector('.section__right_message').textContent = 'Загаданное число меньше вашего!';
-  }
-  else if (guess < secretNumber) {
-    document.querySelector('.section__right_message').textContent = 'Загаданное число больше вашего!';
+    document.querySelector(".section__right_message").textContent =
+      "Нет числа!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".section__right_message").textContent =
+      "Вы угадали!";
+    // document.querySelector(".highscore").textContent += Number(secretNumber);
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".section__right_message").textContent =
+        "Загаданное число меньше вашего!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".section__right_message").textContent =
+        "Попытки закончились! Вы проиграли!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".section__right_message").textContent =
+        "Загаданное число больше вашего!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".section__right_message").textContent =
+        "Попытки закончились! Вы проиграли!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
